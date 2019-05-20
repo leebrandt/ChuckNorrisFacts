209c2be5e61014bf1198f9a65d759f8476ea9de3
Lee Brandt <leebrandt@gmail.com>
2019-05-20 14:52:32 -0500

    Initial commit

---
+++ ChuckNorrisFacts/ChuckNorrisFacts/Fact.cs
@@ -0,0 +1,13 @@
+﻿namespace ChuckNorrisFacts
+{
+
+    public class Fact
+    {
+        public object category { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+
+}
---
+++ ChuckNorrisFacts/ChuckNorrisFacts/LoginView.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Okta.Auth.Sdk;
+using Okta.Sdk.Abstractions.Configuration;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChuckNorrisFacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView : ContentView
+    {
+        public event EventHandler<bool> LoginChanged;
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void SignupClicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://dev-846291.oktapreview.com/signin/register"));
+        }
+
+        private async void LoginClicked(object sender, EventArgs e)
+        {
+            var success = await Login(EmailEntry.Text, PasswordEntry.Text);
+            if (LoginChanged != null) LoginChanged(this, success);
+            if (success)
+            {
+                LoginPanel.IsVisible = false;
+                LogoutPanel.IsVisible = true;
+                ErrorLabel.Text = "";
+            }
+            else
+            {
+                ErrorLabel.Text = "Login failed.";
+            }
+        }
+
+        private void LogoutClicked(object sender, EventArgs e)
+        {
+            LoginPanel.IsVisible = true;
+            LogoutPanel.IsVisible = false;
+            if (LoginChanged != null) LoginChanged(this, false);
+        }
+
+
+        public static async Task<bool> Login(string email, string password)
+        {
+            var config = new OktaClientConfiguration { OktaDomain = "https://dev-846291.oktapreview.com", };
+            var authClient = new AuthenticationClient(config);
+
+            var authnOptions = new AuthenticateOptions { Username = email, Password = password };
+            try
+            {
+                var authnResponse = await authClient.AuthenticateAsync(authnOptions);
+                return authnResponse.AuthenticationStatus == AuthenticationStatus.Success;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
